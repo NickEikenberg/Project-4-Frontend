8481b968600d5810d1c3b401169caebe6bc0ea43
dommentee <chuku0929@gmail.com>
2021-11-17 11:39:20 -0500

    added react and axios

---
+++ app-front/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className="main_container">
+      <h3>Tell me how great your day was</h3>
+    </div>
+  )
+}
+
+export default App;
