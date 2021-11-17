c59779c817ec67c7f434eac0c1c83129482e7be2
dommentee <chuku0929@gmail.com>
2021-11-17 12:04:29 -0500

    deleted svg added Css folder

--- app-front/src/App.js
+++ app-front/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import Add from './components/Add'
+
 const App = () => {
   return (
     <div className="main_container">
       <h3>Tell me how great your day was</h3>
+      <Add />
     </div>
   );
 };
