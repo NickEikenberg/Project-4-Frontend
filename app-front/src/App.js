f5bc7569ffb9043114565d7d56713f7fcdf5c6c1
dommentee <chuku0929@gmail.com>
2021-11-18 17:53:31 -0500

    merged file App.js

--- app-front/src/App.js
+++ app-front/src/App.js
@@ -9,16 +9,10 @@ const App = () => {
 
   return (
     <div className="main_container">
-<<<<<<< HEAD
-      <Header />
-      <Add />
-      <Messages />
-=======
       <Header isAdding={isAdding} setIsAdding={setIsAdding} />
       <h3>Tell me how great your day was</h3>
       <Messages></Messages>
       {isAdding ? <Add /> : null}
->>>>>>> e1fef97281a658ad4bb08f4d2faa590da7af6890
     </div>
   );
 };
