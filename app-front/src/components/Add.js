401cbb01a0cfce92e2370a56c8ce9e4c891be384
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-11-18 17:32:52 -0500

    Created simple add message form

--- app-front/src/components/Add.js
+++ app-front/src/components/Add.js
@@ -2,6 +2,22 @@ import React from 'react';
 // import '../util/css/add.css';
 
 const Add = () => {
-  return <h4>Add form</h4>;
+  return (
+    <div>
+      <h1>SUBMIT YOUR GOOD MOMENT</h1>
+
+      <div className="goodThingForm">
+        <form>
+          <label htmlFor="name">Name: </label>
+          <input type="text" name="name" placeholder="Your Name" />
+          <textarea
+            name="message"
+            placeholder="One good thing that happened today was..."
+          ></textarea>
+          <input type="submit" value="submit"></input>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default Add;
