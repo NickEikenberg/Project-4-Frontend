016fb30551f4e9cf4f61cc7a75c419cf95bb9567
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-11-18 17:39:39 -0500

    Added character count to form

--- app-front/src/components/Add.js
+++ app-front/src/components/Add.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import '../util/css/add.css';
 
 const Add = () => {
+  let [characterCount, setCharacterCount] = useState(280);
+
+  const getCharacterCount = () => {
+    const text = document.getElementById('textarea');
+    let chars = 280;
+    setCharacterCount(chars - text.value.length);
+  };
+
   return (
     <div>
       <h1>SUBMIT YOUR GOOD MOMENT</h1>
@@ -11,11 +19,14 @@ const Add = () => {
           <label htmlFor="name">Name: </label>
           <input type="text" name="name" placeholder="Your Name" />
           <textarea
+            id="textarea"
             name="message"
             placeholder="One good thing that happened today was..."
+            onChange={getCharacterCount}
           ></textarea>
           <input type="submit" value="submit"></input>
         </form>
+        <h1>Character count: {characterCount}</h1>
       </div>
     </div>
   );
