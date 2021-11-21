ac71ed71ab54bf7d5ecebcf2da0781f407c8e7de
dommentee <chuku0929@gmail.com>
2021-11-21 15:08:43 -0500

    centered edit form

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -25,9 +25,9 @@ const Edit = (props) => {
   };
 
   return (
-    <div>
+    <>
       {props.isEditing ? (
-        <div>
+        
           <div className="add_good_thing_container">
             <div className="good_thing_form">
               <div className="add-head">
@@ -67,58 +67,15 @@ const Edit = (props) => {
             <span>Character count: {characterCount}</span>
          </div>
         </div>
-      </div>
+      
       ) : (
           <></>
         )
       }
-    </div>
+    </>
+  )
 
-
-    // <div>
-    //   <div className="add_good_thing_container">
-    //     <div className="good_thing_form">
-    //       <div className="add-head">
-    //         <div className="empty-space"></div>
-    //         <h2>EDIT YOUR GOOD MOMENT</h2>
-    //         <div className="close-modal">X</div>
-    //       </div>
-    //       <form onSubmit={handleSubmit} className="add_thing">
-    //         <label htmlFor="name">Title: </label>
-    //         <input
-    //           type="text"
-    //           name="title"
-    //           className="input"
-    //           onChange={handleChange}
-    //           //   value={message.title}
-    //         />
-    //         <br />
-    //         <br />
-    //         <label htmlFor="name">Message: </label>
-    //         <textarea
-    //           id="textarea"
-    //           name="message"
-    //           rows="6"
-    //           cols="59"
-    //           placeholder={props.message}
-    //           onChange={(event) => {
-    //             getCharacterCount();
-    //             handleChange(event);
-    //           }}
-    //           maxLength="280"
-    //           className="text_area"
-    //           //   value={message.message}
-    //         ></textarea>
-    //         <br />
-    //         <br />
-    //         <input className="send" type="submit" value="SUBMIT"></input>
-    //       </form>
-    //       <br />
-    //       <span>Character count: {characterCount}</span>
-    //     </div>
-    //   </div>
-    // </div>
-  );
+   
 };
 
 export default Edit;
