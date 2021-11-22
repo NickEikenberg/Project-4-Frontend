676b9428acd4db943b5c052280129a532bcfbdb4
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-11-22 02:58:14 -0500

    messages section styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,75 @@
 import React from 'react';
 import '../util/css/footer.css';
 
-
 const Footer = () => {
   return (
-    <div class= "footer">
+    <div class="footer">
       <footer>
         <div class="container">
           <div class="row">
             <div class="column">
               <h3>Find Us on Github</h3>
               <ul>
-                <li><a href="https://github.com/NickEikenberg" target="_blank">Nick Eikenberg</a></li>
-                <li><a href="https://domacor.netlify.app/" target="_blank">Dom Mentee</a></li>
-                <li><a href="https://git.generalassemb.ly/jamiealcazar" target="_blank">Jamie Alcazar</a></li>
+                <li>
+                  <a
+                    href="https://github.com/NickEikenberg"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Nick Eikenberg
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://domacor.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Dom Mentee
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://git.generalassemb.ly/jamiealcazar"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Jamie Alcazar
+                  </a>
+                </li>
               </ul>
             </div>
             <div class="column">
               <h3>Let's Connect</h3>
 
               <ul>
-                <li><a href="https://www.linkedin.com/in/nick-eikenberg/" target="_blank">Nick Eikenberg</a></li>
-                <li><a href="https://www.linkedin.com/in/domacor-mentee-7aa486126/" target="_blank">Dom Mentee</a></li>
-                <li><a href="https://www.linkedin.com/in/jamiealcazar/" target="_blank">Jamie Alcazar</a></li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/nick-eikenberg/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Nick Eikenberg
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/domacor-mentee-7aa486126/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Dom Mentee
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/jamiealcazar/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Jamie Alcazar
+                  </a>
+                </li>
               </ul>
             </div>
             <div class="copyright">
@@ -32,8 +79,7 @@ const Footer = () => {
         </div>
       </footer>
     </div>
-  )
+  );
+};
 
-}
-
-export default Footer
+export default Footer;
